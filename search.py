b19efb6ebfb10c165d7664b261d6f90818e7a6de
Jia Yee <jiayeerawr@gmail.com>
2018-03-17 17:19:11 +0800

    Add code-level documentation
    Signed-off-by: Jia Yee <jiayeerawr@gmail.com>

--- search.py
+++ search.py
@@ -34,27 +34,36 @@ def do_searching(dictionary_file_name, postings_file_name, queries_file_name, ou
         for line in d:
             stem, offset = line.rstrip().split(',')
             offsets[stem] = int(offset)
+        # Load the following data from the lengths file:
+        # 1. Total number of documents in the collection
+        # 2. Length of each document (key is the doc_id)
         N = pickle.load(l)
         lengths_by_document = pickle.load(l)
         # Process each query one-by-one but with the same resources
         # I.e. Duplicate stems are loaded only once
         for line in q:
+            # Similar procedure as slide 38 of w7 lecture
             stems, stemmed_query = get_preprocessed_query(line)
-            query_tfs = Counter(stemmed_query)
+            query_tfs = Counter(stemmed_query) # list of tokens -> {token: frequency}
             tfidf_by_document = {}
             for stem_index, stem in enumerate(stems):
                 query_tfidf = get_tfidf_weight(query_tfs[stem])
-                df, postings = load_stem(stem, p)
-                node = postings.get_head()
+                df, postings = load_stem(stem, p) # {term: (length of skip list, skip list itself)}
+                node = postings.get_head() # i.e. first node of skip list
                 while node is not None:
                     doc_id, doc_tf = node.get_data()
                     tfidf_by_document[doc_id] = get_tfidf_weight(doc_tf, df, N) * query_tfidf
                     node = node.get_next()
-            docs_to_pop = min(len(tfidf_by_document), top_n)
+            docs_to_pop = min(len(tfidf_by_document), top_n) # e.g. min(total relevant docs, 10)
             most_relevant_docs = [-1 for i in range(docs_to_pop)]
+            # We need to negate the tfidf values because Python's built-in heap is strictly min-heap
+            # Since comparison of heap elements is only mutable by overriding __lt__, we simply
+            # re-arrange the tuple so that the value to be compared (the tfidf) is the first value
+            # Tuple comparison is by first value, then second value if first values are equal, and etc
+            # We also do normalization by document length here
             relevant_docs = [(-doc_tfidfs / lengths_by_document[doc_id], doc_id) \
                 for doc_id, doc_tfidfs in tfidf_by_document.items()]
-            heapify(relevant_docs)
+            heapify(relevant_docs) # in-place
             for i in range(docs_to_pop):
                 most_relevant_docs[i] = str(heappop(relevant_docs)[1])
             o.write(' '.join(most_relevant_docs))
