5c3319ea5ee51578960b96e1c8b8b2aca0ef5f14
JIA YEE <jiayeerawr@gmail.com>
2018-04-14 16:49:59 +0800

    Add query expansion (filtered synonym via co-occurrence)
    Signed-off-by: JIA YEE <jiayeerawr@gmail.com>

---
+++ nltk_context_index.py
@@ -0,0 +1,92 @@
+### NOT WRITTEN BY ME. FROM NLTK LIBRARY. SOURCE: http://www.nltk.org/_modules/nltk/text.html ###
+
+from nltk.probability import ConditionalFreqDist as CFD
+
+class ContextIndex(object):
+    """
+    A bidirectional index between words and their 'contexts' in a text.
+    The context of a word is usually defined to be the words that occur
+    in a fixed window around the word; but other definitions may also
+    be used by providing a custom context function.
+    """
+    @staticmethod
+    def _default_context(tokens, i):
+        """One left token and one right token, normalized to lowercase"""
+        left = (tokens[i-1].lower() if i != 0 else '*START*')
+        right = (tokens[i+1].lower() if i != len(tokens) - 1 else '*END*')
+        return (left, right)
+
+    def __init__(self, tokens, context_func=None, filter=None, key=lambda x:x):
+        self._key = key
+        self._tokens = tokens
+        if context_func:
+            self._context_func = context_func
+        else:
+            self._context_func = self._default_context
+        if filter:
+            tokens = [t for t in tokens if filter(t)]
+        self._word_to_contexts = CFD((self._key(w), self._context_func(tokens, i))
+                                     for i, w in enumerate(tokens))
+        self._context_to_words = CFD((self._context_func(tokens, i), self._key(w))
+                                     for i, w in enumerate(tokens))
+
+    def tokens(self):
+        """
+        :rtype: list(str)
+        :return: The document that this context index was
+            created from.
+        """
+        return self._tokens
+
+
+    def word_similarity_dict(self, word):
+        """
+        Return a dictionary mapping from words to 'similarity scores,'
+        indicating how often these two words occur in the same
+        context.
+        """
+        word = self._key(word)
+        word_contexts = set(self._word_to_contexts[word])
+
+        scores = {}
+        for w, w_contexts in self._word_to_contexts.items():
+            scores[w] = f_measure(word_contexts, set(w_contexts))
+
+        return scores
+
+
+    def similar_words(self, word, n=20):
+        scores = defaultdict(int)
+        for c in self._word_to_contexts[self._key(word)]:
+            for w in self._context_to_words[c]:
+                if w != word:
+                    scores[w] += self._context_to_words[c][word] * self._context_to_words[c][w]
+        return sorted(scores, key=scores.get, reverse=True)[:n]
+
+
+    def common_contexts(self, words, fail_on_unknown=False):
+        """
+        Find contexts where the specified words can all appear; and
+        return a frequency distribution mapping each context to the
+        number of times that context was used.
+
+        :param words: The words used to seed the similarity search
+        :type words: str
+        :param fail_on_unknown: If true, then raise a value error if
+            any of the given words do not occur at all in the index.
+        """
+        words = [self._key(w) for w in words]
+        contexts = [set(self._word_to_contexts[w]) for w in words]
+        empty = [words[i] for i in range(len(words)) if not contexts[i]]
+        common = reduce(set.intersection, contexts)
+        if empty and fail_on_unknown:
+            raise ValueError("The following word(s) were not found:",
+                             " ".join(words))
+        elif not common:
+            # nothing in common -- just return an empty freqdist.
+            return FreqDist()
+        else:
+            fd = FreqDist(c for w in words
+                          for c in self._word_to_contexts[w]
+                          if c in common)
+            return fd
--- search.py
+++ search.py
@@ -26,21 +26,32 @@ from constants import (
     print_time
     )
 from skip_list import SkipList
+from nltk_context_index import ContextIndex
+
+# Database for zones
 '''
 conn = sqlite3.connect(database_file_name)
 c = conn.cursor()
 '''
+
 start_time = time()
 
-dictionary = {}
-postings_offsets = {} # { lemma: postings offset }
-nltk_text_offsets = {} # { doc_id: texts offset }
+dictionary = {}         # { lemma: postings skip list}
+postings_offsets = {}   # { lemma: postings offset }
+nltk_texts = {}         # { doc_id: nltk.Text }
+nltk_offsets = {}       # { doc_id: nltk text offset }
+
+# Variables made global because they are read across functions
+N = 0
+lengths_by_document = {}
 
 with open('stopwords.txt') as f:
     stopwords = set(map(lambda ln: ln.strip(), f.readlines()))
 
 # MAIN function for search.py
 def do_searching(dictionary_file_name, postings_file_name, queries_file_name, output_file_name):
+    global N
+    global lengths_by_document
     with open(dictionary_file_name, errors='ignore') as d, \
         open(postings_file_name, 'rb') as p, \
         open(queries_file_name) as q, \
@@ -54,7 +65,7 @@ def do_searching(dictionary_file_name, postings_file_name, queries_file_name, ou
             postings_offsets[lemma] = int(postings_offset)
         for line in i:
             doc_id, nltk_text_offset = line.rstrip().split(',')
-            nltk_text_offsets[doc_id] = int(nltk_text_offset)
+            nltk_offsets[doc_id] = int(nltk_text_offset)
         # Load the following data from the lengths file:
         # 1. Total number of documents in the collection
         # 2. Length of each document (key is the doc_id)
@@ -64,78 +75,126 @@ def do_searching(dictionary_file_name, postings_file_name, queries_file_name, ou
         # I.e. Duplicate lemmas and nltk.Text are loaded only once
         for line in q:
             lemmas, tokens_for_blr, tokens_for_vsm = get_parsed_query(line)
-            # Get all synonyms for each query lemma
-            synonyms = { lemma: list(filter(
-                lambda synonym: synonym != lemma and '_' not in synonym,
-                set(sum(map(lambda synset: synset.lemma_names(), wn.synsets(lemma)), []))))
-            for lemma in lemmas }
-            candidate_length, candidate_skip_list = boolean_retrieve(tokens_for_blr, p)
+
+            # Do boolean retrieval first to separate high list (retrieved) from low list
+            blr_length, blr_skip_list = boolean_retrieve(tokens_for_blr, p)
             query_tfs = Counter(tokens_for_vsm) # list of tokens -> {token: frequency}
-            tfidf_by_document_upp = {}
-            tfidf_by_document_low = {}
-            if candidate_length:
-                for lemma_index, lemma in enumerate(lemmas):
-                    query_tfidf = get_tfidf_weight(query_tfs[lemma])
-                    df, postings = load_lemma(lemma, p)
-                    # BEGIN procedure
-                    # "merge" boolean retrieved postings with postings by term
-                    # Documents which exist in both skip lists will be considered in the upper rankings
-                    # while those which exist only in the term skip list are moved to the lower rankings
-                    node_a = candidate_skip_list.get_head() # i.e. first node of skip list
-                    node_b = postings.get_head()
-                    while node_a is not None and node_b is not None:
-                        data_a = node_a.get_data()
-                        data_b = node_b.get_data()
-                        doc_id, doc_tf = data_b
-                        if data_a != data_b:
-                            tfidf_by_document_low[doc_id] = get_tfidf_weight(doc_tf, df, N) * query_tfidf
-                            if data_a < data_b:
-                                skip_node_a = node_a.get_skip()
-                                if skip_node_a is not None and skip_node_a.get_data() <= data_b:
-                                    node_a = skip_node_a
-                                else:
-                                    node_a = node_a.get_next()
-                            elif data_b < data_a:
-                                skip_node_b = node_b.get_skip()
-                                if skip_node_b is not None and skip_node_b.get_data() <= data_a:
-                                    node_b = skip_node_b
-                                else:
-                                    node_b = node_b.get_next()
-                        else:
-                            tfidf_by_document_upp[doc_id] = get_tfidf_weight(doc_tf, df, N) * query_tfidf
-                            node_a = node_a.get_next()
-                            node_b = node_b.get_next()
-                    # END procedure
+
+            # Get ranked high and low lists via vector space model
+            most_relevant_docs, less_relevant_docs = get_relevant_docs(
+                blr_skip_list, lemmas, query_tfs,  p)
+
+            # BEGIN procedure for query expansion
+            # Get all synonyms for each query lemma
+            synonyms_by_lemma = { lemma: filter(
+                lambda synonym: synonym != lemma and '_' not in synonym,
+                set(sum(map(lambda synset: synset.lemma_names(), wn.synsets(lemma)), [])))
+            for lemma in lemmas }
+
+            if most_relevant_docs:
+                relevant_docs = most_relevant_docs
             else:
-                # Pure vector space model because boolean retrieved postings is empty
-                # Similar procedure to slide 38 of w7 lecture
-                for lemma_index, lemma in enumerate(lemmas):
-                    query_tfidf = get_tfidf_weight(query_tfs[lemma])
-                    df, postings = load_lemma(lemma, p)
-                    node_b = postings.get_head()
-                    while node_b is not None:
-                        doc_id, doc_tf = node_b.get_data()
-                        tfidf_by_document_upp[doc_id] = get_tfidf_weight(doc_tf, df, N) * query_tfidf
-                        node_b = node_b.get_next()
-            most_relevant_docs = list(map(
-                lambda id_tfidf_tuple: str(id_tfidf_tuple[0]),
-                sorted(
-                    tfidf_by_document_upp.items(),
-                    key=lambda id_tfidf_tuple: id_tfidf_tuple[1],
-                    reverse=True)
-                )
-            )
-            less_relevant_docs = map(
-                lambda id_tfidf_tuple: str(id_tfidf_tuple[0]),
-                sorted(
-                    tfidf_by_document_low.items(),
-                    key=lambda id_tfidf_tuple: id_tfidf_tuple[1],
-                    reverse=True)
-                )
+                relevant_docs = less_relevant_docs
+
+            query_expansion = []
+            for lemma, synonyms in synonyms_by_lemma.items():
+                for doc_id in relevant_docs:
+                    nltk_text = load_nltk_text(doc_id, t)
+                    sim_words = set(get_similar(nltk_text, lemma))
+                    query_expansion.extend(sim_words.intersection(synonyms))
+            tokens_for_vsm.extend(query_expansion)
+            query_tfs = Counter(tokens_for_vsm)
+            # END procedure
+
+            # Get ranked high and low lists via vector space model, and expanded query
+            most_relevant_docs, less_relevant_docs = get_relevant_docs(
+                blr_skip_list, lemmas, query_tfs,  p)
+
             most_relevant_docs.extend(less_relevant_docs)
             o.write(' '.join(most_relevant_docs))
             o.write('\n')
             break # because 1 query per file
+
+'''
+This is a procedure after boolean retrieval
+    (abstracted into a function because re-run is needed for query expansion)
+- If there are zero documents from boolean retrieval of query terms,
+    resort to pure vector space model
+- Else assign the returned documents to the high-list and
+    do vector space model on them for ranking
+- Low-list contains documents ranked by vector space model,
+    but fail to be retrieved via boolean retrieval
+    (i.e. one or more query terms not existing in the document)
+'''
+def get_relevant_docs(blr_skip_list, lemmas, query_tfs, p):
+    tfidf_by_document_upp = {}
+    tfidf_by_document_low = {}
+    if blr_skip_list.get_length():
+        for lemma_index, lemma in enumerate(lemmas):
+            query_tfidf = get_tfidf_weight(query_tfs[lemma])
+            df, postings = load_lemma(lemma, p)
+            # BEGIN procedure
+            # "merge" boolean retrieved postings with postings by term
+            # Documents which exist in both skip lists will be considered in the upper rankings
+            # while those which exist only in the term skip list are moved to the lower rankings
+            node_a = blr_skip_list.get_head() # i.e. first node of skip list
+            node_b = postings.get_head()
+            while node_a is not None and node_b is not None:
+                data_a = node_a.get_data()
+                data_b = node_b.get_data()
+                doc_id, doc_tf = data_b
+                if data_a != data_b:
+                    tfidf_by_document_low[doc_id] = get_tfidf_weight(doc_tf, df, N) * query_tfidf
+                    if data_a < data_b:
+                        skip_node_a = node_a.get_skip()
+                        if skip_node_a is not None and skip_node_a.get_data() <= data_b:
+                            node_a = skip_node_a
+                        else:
+                            node_a = node_a.get_next()
+                    elif data_b < data_a:
+                        skip_node_b = node_b.get_skip()
+                        if skip_node_b is not None and skip_node_b.get_data() <= data_a:
+                            node_b = skip_node_b
+                        else:
+                            node_b = node_b.get_next()
+                else:
+                    tfidf_by_document_upp[doc_id] = get_tfidf_weight(doc_tf, df, N) * query_tfidf
+                    node_a = node_a.get_next()
+                    node_b = node_b.get_next()
+            # END procedure
+    else:
+        # Pure vector space model because boolean retrieved postings is empty
+        # Similar procedure to slide 38 of w7 lecture
+        for lemma_index, lemma in enumerate(lemmas):
+            query_tfidf = get_tfidf_weight(query_tfs[lemma])
+            df, postings = load_lemma(lemma, p)
+            node_b = postings.get_head()
+            while node_b is not None:
+                doc_id, doc_tf = node_b.get_data()
+                tfidf_by_document_upp[doc_id] = get_tfidf_weight(doc_tf, df, N) * query_tfidf
+                node_b = node_b.get_next()
+    # Normalization
+    tfidf_by_document_upp = { doc_id: tfidf / lengths_by_document[doc_id] \
+        for doc_id, tfidf in tfidf_by_document_upp.items() }
+    tfidf_by_document_low = { doc_id: tfidf / lengths_by_document[doc_id] \
+        for doc_id, tfidf in tfidf_by_document_low.items() }
+    # Sort dictionary by descending normalized tfidf, and return lists of doc_id
+    most_relevant_docs = list(map(
+        lambda id_tfidf_tuple: str(id_tfidf_tuple[0]),
+        sorted(
+            tfidf_by_document_upp.items(),
+            key=lambda id_tfidf_tuple: id_tfidf_tuple[1],
+            reverse=True)
+        )
+    )
+    less_relevant_docs = map(
+        lambda id_tfidf_tuple: str(id_tfidf_tuple[0]),
+        sorted(
+            tfidf_by_document_low.items(),
+            key=lambda id_tfidf_tuple: id_tfidf_tuple[1],
+            reverse=True)
+        )
+    return (most_relevant_docs, less_relevant_docs)
 
 '''
 Boolean retrieval routine (AND only)
@@ -204,6 +263,19 @@ def load_lemma(lemma, postings_file_object):
     dictionary[lemma] = (postings.get_length(), postings)
     return dictionary[lemma]
 
+# Accepts a doc_id, a nltk text file handle, and
+# Returns the loaded nltk.Text while storing it in memory
+def load_nltk_text(doc_id, nltk_texts_file_object):
+    global nltk_texts
+    if doc_id in nltk_texts:
+        return nltk_texts[doc_id]
+    text = ''
+    if doc_id in nltk_offsets:
+        nltk_texts_file_object.seek(nltk_offsets[doc_id])
+        text = pickle.load(nltk_texts_file_object)
+    nltk_texts[doc_id] = text
+    return nltk_texts[doc_id]
+
 def get_tfidf_weight(tf, df=0, N=0):
     tf_weight = 0
     idf_weight = 1
@@ -212,6 +284,29 @@ def get_tfidf_weight(tf, df=0, N=0):
     if df:
         idf_weight = log10(N / df)
     return tf_weight * idf_weight
+
+# Adapted from: http://www.nltk.org/_modules/nltk/text.html
+def get_similar(nltk_text, word, num=20):
+    """
+    Distributional similarity: find other words which appear in the
+    same contexts as the specified word; list most similar words first.
+
+    :param word: The word used to seed the similarity search
+    :type word: str
+    :param num: The number of words to generate (default=20)
+    :type num: int
+    :seealso: ContextIndex.similar_words()
+    """
+    if '_word_context_index' not in nltk_text.__dict__:
+        nltk_text._word_context_index = ContextIndex(
+        nltk_text.tokens, filter=lambda x: x.isalpha(), key=lambda s: s.lower())
+    word = word.lower()
+    wci = nltk_text._word_context_index._word_to_contexts
+    if word in wci.conditions():
+        contexts = set(wci[word])
+        fd = Counter(w for w in wci.conditions() for c in wci[w] if c in contexts and not w == word)
+        return [w for w, _ in fd.most_common(num)]
+    return []
 
 def usage():
     print('Usage: ' + sys.argv[0] + ' -d dictionary-file -p postings-file -q query-file -o output-file-of-results')
