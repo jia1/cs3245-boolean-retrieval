1ef14a85ae40440a396a63dfc42d264734e0eece
JIA YEE <jiayeerawr@gmail.com>
2018-04-14 15:32:07 +0800

    Index nltk.Text
    Signed-off-by: JIA YEE <jiayeerawr@gmail.com>

--- constants.py
+++ constants.py
@@ -4,6 +4,8 @@ zones_table_name = 'zones'
 
 # lengths_file_name = 'intellex_indexes\\lengths.txt'
 lengths_file_name = 'lengths.txt'
+nltk_offsets_file_name = 'offsets.txt'
+nltk_texts_file_name = 'texts.txt'
 and_operator_name = 'and'
 
 def print_time(start_time, stop_time):
--- index.py
+++ index.py
@@ -14,14 +14,25 @@ import sqlite3
 from collections import Counter
 from time import time
 
+from nltk import Text
+from nltk.corpus import PlaintextCorpusReader
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 
-from constants import lengths_file_name, print_time, database_file_name, zones_table_name
+from constants import (
+    lengths_file_name,
+    nltk_offsets_file_name,
+    nltk_texts_file_name,
+    database_file_name,
+    zones_table_name,
+    print_time
+    )
 from skip_list import SkipList
 
 nltk.download('wordnet')
+
+# Database for zones
 '''
 conn = sqlite3.connect(database_file_name)
 c = conn.cursor()
@@ -31,6 +42,7 @@ c.execute('CREATE TABLE {} (document_id INTEGER, title TEXT, date_posted TEXT, c
     .format(zones_table_name))
 conn.commit()
 '''
+
 # Adapted from: https://stackoverflow.com/a/15063941
 max_int = sys.maxsize
 should_decrement = True
@@ -54,6 +66,7 @@ Create a dictionary[lemma] -> sorted([(doc id, tf)]) and write to the dictionary
 def do_indexing(csv_file_path, dictionary_file_name, postings_file_name):
     dictionary = {}
     lengths_by_document = {}
+    nltk_texts_by_document = {}
     seen_postings_by_lemma = {}
     with open(csv_file_path, 'r', errors='ignore') as f:
         reader = csv.reader(f)
@@ -62,16 +75,17 @@ def do_indexing(csv_file_path, dictionary_file_name, postings_file_name):
         # columns expected value = ['document_id', 'title', 'content', 'date_posted', 'court']
         columns = list(filter(None, next(reader)))
         N = 0
-        for csv_row in reader: # Uncomment this line if not testing
-        # for i in range(100): # Comment this line if not testing
-            # csv_row = next(reader) # Comment this line if not testing
+        # for csv_row in reader: # Uncomment this line if not testing
+        for i in range(100): # Comment this line if not testing
+            csv_row = next(reader) # Comment this line if not testing
             N += 1
             document_id, title, content, date_posted, court = csv_row
             # BEGIN procedure index content (i.e. vector space model indexing)
-            text = get_preprocessed(content)
+            nltk_text, text_counter = get_preprocessed(content)
             posting = int(document_id)
-            lengths_by_document[posting] = sum(text.values())
-            for lemma, frequency in text.items():
+            lengths_by_document[posting] = sum(text_counter.values())
+            nltk_texts_by_document[posting] = nltk_text
+            for lemma, frequency in text_counter.items():
                 posting_frequency_tuple = (posting, frequency)
                 if lemma not in dictionary:
                     dictionary[lemma] = [posting_frequency_tuple]
@@ -94,25 +108,30 @@ def do_indexing(csv_file_path, dictionary_file_name, postings_file_name):
     with open(lengths_file_name, 'wb') as l:
         pickle.dump(N, l)
         pickle.dump(lengths_by_document, l)
+    with open(nltk_offsets_file_name, 'w') as i, open(nltk_texts_file_name, 'wb') as t:
+        for doc_id, nltk_text in nltk_texts_by_document.items():
+            i.write('{doc_id},{offset}\n'.format(doc_id=doc_id, offset=t.tell()))
+            pickle.dump(nltk_text, t)
 
 '''
 Preprocess a text string in the following order:
-    1. (L77) Do sentence tokenization
-    2. (L75) For each sentence, do case-folding, word tokenization
-    3. (L73) Filter out punctuations, non-alphabetical words, and stopwords
-    4. (L72) Lemmatize the remaining words
-    5. (L78) Return a Counter of lemmatized words (i.e. {lemma: frequency})
+    1. Do word tokenization
+    2. Do case-folding
+    3. Filter out punctuations, non-alphabetical words, and stopwords
+    4. Lemmatize the remaining words
+    5. Return a tuple of nltk.Text(output from step 1) and
+        Counter of lemmatized words from step 4 (i.e. {lemma: frequency})
 '''
 def get_preprocessed(text):
-    sentences = map(
-        lambda sentence: list(map(
-            lambda word: lemmatizer.lemmatize(word),
-            filter(
-                lambda token: token not in string.punctuation and token.isalpha() and token not in stopwords,
-                word_tokenize(sentence.lower())
-                ))),
-        sent_tokenize(text))
-    return Counter((lemma for lemmas in sentences for lemma in lemmas))
+    tokens = word_tokenize(text.strip())
+    tokens_to_index = map(
+        lemmatizer.lemmatize,
+        filter(
+            lambda token: token not in string.punctuation and token.isalpha() and token not in stopwords,
+            map(str.lower, tokens)
+            )
+        )
+    return (Text(tokens), Counter(tokens_to_index))
 
 def usage():
     print('Usage: ' + sys.argv[0] + ' -i dataset-file -d dictionary-file -p postings-file')
